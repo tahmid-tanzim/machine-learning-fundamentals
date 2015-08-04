010a81c51649c6ba4add2d86cfac14b3913094e6
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2015-08-04 18:07:07 +0600

    Completed 09-03 Annotation Based Aspects

--- src/com/born2code/spring/aop/Camera.java
+++ src/com/born2code/spring/aop/Camera.java
@@ -1,5 +1,8 @@
 package com.born2code.spring.aop;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Camera {
     public void snap() {
         System.out.println("SNAP");
--- src/com/born2code/spring/aop/Logger.java
+++ src/com/born2code/spring/aop/Logger.java
@@ -1,10 +1,24 @@
 package com.born2code.spring.aop;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class Logger {
+
+    @Pointcut("execution(void com.born2code.spring.aop.Camera.snap())")
+    public void reusablePointCut(){}
+
+    @Before("reusablePointCut()")
     public void aboutToTakePhoto() {
         System.out.println("About To Take Photo...");
     }
 
+    @After("reusablePointCut()")
     public void theEnd() {
         System.out.println("Bye...");
     }

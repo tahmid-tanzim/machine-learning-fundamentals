180eaab7265a84ca6cc90fc749820d65175eacd3
Tahmid Tanzim Lupin <ttanzimlupin@presto.com>
2024-10-02 13:27:46 -0600

    Generative-AL with LLM: Huggingface dataset load

---
+++ generative-ai-with-llms/main.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
+
+if __name__ == "__main__":
+    huggingface_dataset_name = "knkarthick/dialogsum"
+    dataset = load_dataset(huggingface_dataset_name)
+
+    example_indices = [40, 200]
+    dash_line = '-'.join('' for x in range(100))
+    for i, index in enumerate(example_indices):
+        print(dash_line)
+        print('Example ', i + 1)
+        print(dash_line)
+        print('INPUT DIALOGUE:')
+        print(dataset['test'][index]['dialogue'])
+        print(dash_line)
+        print('BASELINE HUMAN SUMMARY:')
+        print(dataset['test'][index]['summary'])
+        print(dash_line, end='\n')
+

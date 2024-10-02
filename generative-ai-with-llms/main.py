9a90c8a907136868fef24022cd692332384e64f2
Tahmid Tanzim Lupin <ttanzimlupin@presto.com>
2024-10-02 13:50:25 -0600

    Generative-AL with LLM: load model & tokenizer and test encoding & decoding

--- generative-ai-with-llms/main.py
+++ generative-ai-with-llms/main.py
@@ -2,9 +2,11 @@ from datasets import load_dataset
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, GenerationConfig
 
 if __name__ == "__main__":
+    # Loading Datasets
     huggingface_dataset_name = "knkarthick/dialogsum"
     dataset = load_dataset(huggingface_dataset_name)
 
+    # Viewing Datasets
     example_indices = [40, 200]
     dash_line = '-'.join('' for x in range(100))
     for i, index in enumerate(example_indices):
@@ -18,3 +20,19 @@ if __name__ == "__main__":
         print(dataset['test'][index]['summary'])
         print(dash_line, end='\n')
 
+    # Loading Model
+    model_name = "google/flan-t5-base"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    # Loading Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+
+    # Test Encoding & Decoding
+    sentence = "What time is it, Tom?"
+    sentence_encoded = tokenizer(sentence, return_tensors="pt")
+    sentence_decoded = tokenizer.decode(
+        sentence_encoded["input_ids"][0],
+        skip_special_tokens=True
+    )
+    print("ENCODED SENTENCE:\n", sentence_encoded["input_ids"][0], end="\n")
+    print("DECODED SENTENCE:\n", sentence_decoded, end="\n")

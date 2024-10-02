92827b0eaf220eef9bc2b318220b629483101c4f
Tahmid Tanzim Lupin <ttanzimlupin@presto.com>
2024-10-02 14:43:20 -0600

    Generative-AL with LLM: Zero Shot Prompt.

--- generative-ai-with-llms/main.py
+++ generative-ai-with-llms/main.py
@@ -23,11 +23,9 @@ class SummarizeDialogue:
             print(self.dash_line)
             print('Example ', i + 1)
             print(self.dash_line)
-            print('INPUT DIALOGUE:')
-            print(self.dataset['test'][index]['dialogue'])
+            print('INPUT DIALOGUE:\n', self.dataset['test'][index]['dialogue'], end='\n')
             print(self.dash_line)
-            print('BASELINE HUMAN SUMMARY:')
-            print(self.dataset['test'][index]['summary'])
+            print('BASELINE HUMAN SUMMARY:\n', self.dataset['test'][index]['summary'], end='\n')
             print(self.dash_line, end='\n')
 
     def test_encoding_decoding(self, sentence="What time is it, Tom?"):
@@ -55,15 +53,37 @@ class SummarizeDialogue:
             print(self.dash_line)
             print('Example ', i + 1)
             print(self.dash_line)
-            print('INPUT PROMPT:')
-            print(dialogue)
+            print('INPUT PROMPT:\n', dialogue, end='\n')
             print(self.dash_line)
-            print('BASELINE HUMAN SUMMARY:')
-            print(summary)
-            print('MODEL GENERATION - WITHOUT PROMPT ENGINEERING:')
-            print(decoded_dialogue, end='\n')
+            print('BASELINE HUMAN SUMMARY:\n', summary, end='\n')
+            print('MODEL GENERATION - WITHOUT PROMPT ENGINEERING:\n', decoded_dialogue, end='\n')
+
+    def summarize_dialogue_with_zero_shot_inference(self):
+        for i, index in enumerate(self.example_indices):
+            dialogue = self.dataset['test'][index]['dialogue']
+            summary = self.dataset['test'][index]['summary']
+
+            prompt = f"Summarize the following conversation.\n{dialogue}\nSummary:\n"
+
+            encoded_dialogue = self.tokenizer(prompt, return_tensors="pt")
+            decoded_dialogue = self.tokenizer.decode(
+                self.model.generate(
+                    encoded_dialogue["input_ids"],
+                    max_new_tokens=50,
+                )[0],
+                skip_special_tokens=True
+            )
+            print(self.dash_line)
+            print('Example ', i + 1)
+            print(self.dash_line)
+            print('INPUT PROMPT:\n', prompt, end='\n')
+            print(self.dash_line)
+            print('BASELINE HUMAN SUMMARY:', summary, end='\n')
+            print('MODEL GENERATION - ZERO SHOT:', decoded_dialogue, end='\n')
 
 
 if __name__ == "__main__":
     sd = SummarizeDialogue()
+    # sd.view_dataset()
     sd.summarize_dialogue_without_prompt_engineering()
+    sd.summarize_dialogue_with_zero_shot_inference()
